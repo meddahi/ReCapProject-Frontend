eaf6040b966e0bab54548d458452163bde7be920
meddahi <cahduran13043@gmail.com>
2021-03-20 16:30:31 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { from } from 'rxjs';
+import { UserComponent } from './components/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    NaviComponent,
+    RentalComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/userResponseModel.ts
@@ -0,0 +1,8 @@
+  
+
+import { ResponseModel } from "./responseModel";
+import { User } from "./user";
+
+export interface UserResponseModel extends ResponseModel{
+    data:User[];
+}
